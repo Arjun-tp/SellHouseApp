a083471a2a9ab2b1bdf0079ca9235ea6db35b2bb
Arjun-tp <arjuntpnambiar@gmail.com>
2021-11-21 19:09:56 -0500

    Register Screen

--- app/src/main/java/com/example/sellhouse/SignScreenActivity.java
+++ app/src/main/java/com/example/sellhouse/SignScreenActivity.java
@@ -2,13 +2,36 @@ package com.example.sellhouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignScreenActivity extends AppCompatActivity {
-
+    Button signIn, signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_screen);
+        signIn = findViewById(R.id.btnSignIn);
+        signUp = findViewById(R.id.btnSignUp);
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignScreenActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignScreenActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
