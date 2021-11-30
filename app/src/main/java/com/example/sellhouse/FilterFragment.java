a0a32576f8dd5dee0e75601420acdbe239b817a8
Arjun-tp <arjuntpnambiar@gmail.com>
2021-11-30 02:25:17 -0500

    Filter screen

---
+++ app/src/main/java/com/example/sellhouse/FilterFragment.java
@@ -0,0 +1,18 @@
+package com.example.sellhouse;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class FilterFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_filter, container, false);
+        getActivity().setTitle("Filter");
+
+
+        return view;
+    }
+}
